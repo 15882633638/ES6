d75a71167cf9418d16c4896dc22d9eaf68d93416
unknown <741097812@qq.com>
2020-07-15 09:13:23 +0800

    增加Proxy文件

---
+++ Proxy.js
@@ -0,0 +1,42 @@
+//主要是为了操作对象，以前的denfineproperty是操作对象的单个属性
+//Proxy现在可以操作对象的所有属性,由两部分组成，target(目标)，handler(拦截器)
+// target是目标对象，handler是拦截器
+//对象
+let obj = {
+    name:'haha',
+    gender:true,
+    age:19
+}
+//拦截器
+let handler = {
+    get(target,key){
+        console.log(`getting${key}`);
+        return target[key];
+    },
+    set(target,key,newvalue){
+        console.log(`setting${key}`);
+        target[key]=value;
+    }
+}
+//第一种写法
+// let proxy = new Proxy(obj,handler);
+//第二种写法
+let proxy = new Proxy(obj,{
+    get(target,key,receiver){
+        if(key[0]==='_'){
+            throw new Error(`没有这样的属性`);
+        }
+        console.log(`getting${key}`);
+        console.log(receiver);
+        return target[key];
+    },
+    set(target,key,newvalue){
+        console.log(`setting${key}`);
+        target[key]=value;
+    }
+});
+console.log(proxy.a)
+
+
+
+
